d450b773892144e114b6977bffaf3505d9e1830d
kazipintu <kazipintu@gmail.com>
2024-05-12 20:03:48 +0530

    MERN/eShikhon/EC6/Default/Value/For/Function/Parameter

---
+++ default-value.js
@@ -0,0 +1,71 @@
+{
+  function addition(num1, num2) {
+    const total = num1 + num2;
+    //console.log(num1, num2);
+    //console.log(total);
+
+  }
+  const result = addition(15, 2);
+
+}
+
+// PROBLEM:- total is coming as Not any Number (NaN)
+{
+  function addition(num1, num2) {
+    const total = num1 + num2;
+    console.log(num1, num2);
+    console.log(total);
+
+  }
+  const result = addition(15,);
+
+}
+
+//SoLUTION:-
+
+/* ~ OPTION-3: "EC6 latest System" to solve the problem ~  */
+{
+  function addition(num1, num2 = 0) {
+    // setting default value of num2 = 0, now num2 can be executable by any value assigned in it
+    console.log(num1, num2);
+    const total = num1 + num2;
+    console.log(total);
+
+  }
+  const result = addition(15, 5);
+
+}
+
+
+/* ~ OPTION-2: Old System to solve the problem ~  */
+{
+  function addition(num1, num2) {
+    console.log(num1, num2);
+
+    num2 = num2 || 0;
+    const total = num1 + num2;
+    console.log(num1, num2);
+    console.log(total);
+
+  }
+  const result = addition(15,);
+
+}
+
+
+/* ~ OPTION-1: Older System to solve the problem ~  */
+{
+  function addition(num1, num2) {
+    console.log(num1, num2);
+
+    if (num2 == undefined) {
+      num2 = 0;
+    }
+    const total = num1 + num2;
+    console.log(num1, num2);
+    console.log(total);
+
+  }
+  const result = addition(15,);
+
+}
